40544c902def630cb5405539711fcd16faaac29b
Luigj_Lazri <lazri.luigj@gmail.com>
2025-10-20 10:22:38 +0200

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { TodoList } from './todos/todo-list/todo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'todos', component: TodoList },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // default route
+];
+
